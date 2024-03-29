465764ec4c190f2a9e7c9a1ff694dc8d1f6f76e9
Sanjay radadiya <sanjay.radadiya91@gmail.com>
2024-03-29 19:56:25 +0530

    update dist

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,11 +1,30 @@
-import { FC } from 'react';
+import { CSSProperties, FC } from 'react';
 
 type PickerType = "date" | "time" | "dateTime";
-interface PickersProps {
+interface PickersProps extends PickerBoxStylesProps {
     onChange?: (data: string) => void;
     value: string;
+    /**
+     * Format string to customize the display format of the value.
+     * The format string follows Luxon's formatting syntax.
+     * Input and output formatting will be same.
+     * And value formate must be same as pass formate.
+     * Default formate(date: yyyy-LL-dd (Iso date formate), time: hh:mm:ss a, dateTime: yyyy-LL-dd hh:mm:ss a)
+     * @example "yyyy-LL-dd hh:mm:ss a Z" (for reference to Luxon formatting, You can also handle zone)
+     */
     format?: string;
     show?: boolean;
+    className?: string;
+}
+/**
+ * Props for customizing the styles of the main container component that contains all the pickers.
+ */
+interface PickerBoxStylesProps {
+    mainContainerClassName?: string;
+    mainContainerStyles?: CSSProperties;
+}
+interface TimeComponentProps extends PickersProps {
+    selectedTimeStyle?: CSSProperties;
 }
 interface InputPickerProps extends Omit<PickersProps, "show"> {
     type: PickerType;
@@ -17,7 +36,7 @@ declare const InputPicker: FC<InputPickerProps>;
 
 declare const DatePicker: FC<PickersProps>;
 
-declare const TimePicker: FC<PickersProps>;
+declare const TimePicker: FC<TimeComponentProps>;
 
 declare const DateTimePicker: FC<PickersProps>;
 
